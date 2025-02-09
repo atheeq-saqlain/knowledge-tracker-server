811c437e8826c436dd223ed7ff58f87e9046ff7a
atheeq <atheeqsaqlain@gmail.com>
2025-02-09 19:52:49 +0530

    added extra details for student

--- modules/students/data-access/students.model.js
+++ modules/students/data-access/students.model.js
@@ -5,22 +5,51 @@ const studentSchema = new Schema({
   name: {
     type: String,
   },
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid email!`,
+    },
+  },
+  dateOfBirth: {
+    type: Date,
+  },
+  grade: {
+    type: String,
+  },
   institutes: [
     {
       type: Schema.ObjectId,
       ref: 'Institute',
     },
   ],
-  teachers: [
+  teachersAssigned: [
     {
-      type: Schema.ObjectId,
-      ref: 'User',
+      teacher: {
+        type: Schema.ObjectId,
+        ref: 'User',
+      },
+      syllabus: {
+        type: Schema.ObjectId,
+        ref: 'Syllabus',
+      },
+      assignedDate: Date,
+      institute: {
+        type: Schema.ObjectId,
+        ref: 'Institute',
+      },
     },
   ],
-  syllabus: {
-    type: Schema.ObjectId,
-    ref: 'Syllabus',
-  },
+  syllabus: [
+    {
+      type: Schema.ObjectId,
+      ref: 'Syllabus',
+    },
+  ],
   assignments: [{}], // make a seperate collection
   // exams: [],
   concepts: [
@@ -40,6 +69,17 @@ const studentSchema = new Schema({
         },
         // other metrics of performance and other meta data
       ],
+    },
+  ],
+  questionsAttempted: [
+    {
+      question: {
+        type: Schema.ObjectId,
+        ref: 'Question',
+      },
+      score: Number,
+      maxScore: Number,
+      dateAttempted: Date,
     },
   ],
 });
